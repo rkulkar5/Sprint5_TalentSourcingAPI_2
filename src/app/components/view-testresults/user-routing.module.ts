23c89ecca0e3ff09997601615444f59cd1dd395e
Rajesh <rkulkar5@in.ibm.com>
2020-06-01 15:44:35 +0530

    add

--- src/app/components/view-testresults/user-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import { ViewTestresultsComponent } from './view-testresults.component';
-
-const routes: Routes = [
-  { path: '', component: ViewTestresultsComponent }
-];
-
-export const UserRoutes = RouterModule.forChild(routes);
-
