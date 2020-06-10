b8f3cd6d77dd6e470e7f7c98ed4d6d49696d14f2
rkulkar5 <rkulkar5@in.ibm.com>
2020-06-10 11:21:45 +0530

    Update preTechForm.route.js

--- routes/preTechForm.route.js
+++ routes/preTechForm.route.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const preTechAssessmentForm = express.Router();
+const preTechAssessmentRoute = express.Router();
 
 //Pretechassesmentanswer model
 let PreTechAssessmentAnswer = require('../models/PreTechAssessmentAnswer');
 //PreTechQuestionnaire model
 let PreTechQuestionnaire = require('../models/PreTechQuestionnaire');
 
+let Results = require('../models/Results');
+
+
 // Add QuestionBank
-preTechAssessmentForm.route('/saveDraftQuestions').post((req, res, next) => {
+preTechAssessmentRoute.route('/saveDraftQuestions').post((req, res, next) => {
   QuestionBank.create(req.body, (error, data) => {
     if (error) {
       return next(error)
@@ -20,20 +23,70 @@ preTechAssessmentForm.route('/saveDraftQuestions').post((req, res, next) => {
 
 
   
-// Get pretechnical questions based on jrss
-preTechAssessmentForm.route('/getPreTechQuestionanire/:jrss').get((req, res) => {
-console.log('req.params.jrss  **** ', req.params.jrss);
-  PreTechQuestionnaire.find({jrss: req.params.jrss}, function(err,PreTechQuestions){
-  console.log('PreTechQuestions **** ', PreTechQuestions);
-      if(err){
-        console.log(err);
-        return res.status(500).send('');
-      }
-      if(!PreTechQuestions){
-        return res.status(404).send();
-      }
-      return res.json(PreTechQuestions);
+  // Get pre technical assessment questions based on jrss
+preTechAssessmentRoute.route('/getPreTechQuestionanire/:jrss/:userName').get((req, res, next) => {
+  PreTechQuestionnaire.aggregate([
+  {$match : { jrss:req.params.jrss}},
+  { $lookup: { from: "PreTechAssessmentAnswer", 
+  let: { qid: "$preTechQID" }, 
+  pipeline: [{ $match: { $expr: { $and: [
+    { $eq: ["$$qid", "$preTechQID"] }, 
+    { $eq: ["$userName", req.params.userName] }] } } }, 
+    { $project: {_id:0,  answer: 1, status:1 } }], as: "PreTechAnswers" } }],(error, data) => {
+      if (error) {
+          return next(error)
+        } else {
+          res.json(data)
+        }
+      })
     })
-  });
   
-module.exports = preTechAssessmentForm;
+  
+  
+  
+ preTechAssessmentRoute.route('/saveOrSubmit').put((req, res, next) => {
+ 
+	 req.body.forEach((answer) => {
+		//console.log("*********8req body *******",answer);
+		  PreTechAssessmentAnswer.findOneAndUpdate(
+			{preTechQID: answer.preTechQID}, 
+			{ $set:{preTechQID:answer.preTechQID,userName:answer.userName,answer:answer.answer,status:answer.status, createdDate:new Date() }},
+			{upsert: true, new: true, runValidators: true},
+			function (err, doc) { 
+				if (err) {
+					// handle error
+				} else {
+					// handle document
+				}
+			}
+		)
+	 });
+}) 
+
+
+  // Get  workflow stage statuses for a given userName
+preTechAssessmentRoute.route('/getStageStatus/:userName').get((req, res, next) => {
+  Results.findOne({userName: req.params.userName, userScore: { $gt: Number(80) }}, function(error,data){
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+});
+
+
+// Update the status of 2nd stage to 'Completed' 
+preTechAssessmentRoute.route('/updateStage2Status/:userName').post((req, res, next) => {
+ Results.updateOne({userName: req.params.userName, userScore: { $gt: Number(80) } },
+					{ $set: {stage2_Status: "Completed"}}
+      , function(error, data) {
+        if (error) {
+          return next(error);
+        } else {
+          res.json(data)
+        }
+      })
+    });
+
+module.exports = preTechAssessmentRoute;
