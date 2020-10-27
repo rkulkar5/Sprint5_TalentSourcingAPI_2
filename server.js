4d8e2446639c8d9d32b327027068c19e5999eb9d
rkulkar5 <rkulkar5@in.ibm.com>
2020-10-28 00:01:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -117,8 +117,8 @@ app.use((req, res, next) => {
 
 // error handler
 app.use(function (err, req, res, next) {
-  console.error(err.message); // Log error message in our server's console
-  if (!err.statusCode) {err.statusCode = 500;
+  console.error(err.message + ` ******* `+ err); // Log error message in our server's console
+  if (!err.statusCode) {err.statusCode = 700;
   } // If err has no specified error code, set error code to 'Internal Server Error (500)'
   res.status(err.statusCode).send(err.message); // All HTTP requests must have a response, so let's send back an error with its status code and message
 });
